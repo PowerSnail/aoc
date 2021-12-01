a0d5b2e1c27143e343e351ea454c69a65a273f00
PowerSnail <hj@powersnail.com>
2021-12-01 09:52:04 -0800

    day1part1

---
+++ run.py
@@ -0,0 +1,8 @@
+import subprocess as sp
+import sys
+
+day = int(sys.argv[1])
+part = int(sys.argv[2])
+
+sp.run("cargo build --release", shell=True, check=True, stdout=sp.PIPE, stderr=sp.PIPE)
+sp.run(f"target/release/aoc_2021 {day} {part} <inputs/day{day}.txt | tee outputs/day{day}-part{part}.txt", shell=True, check=True)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use itertools::Itertools;
+use std::io::{self, prelude::*};
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(version = "1.0")]
+struct Opts {
+    day: u32,
+    part: u32,
+}
+
+fn day1_part1(lines: impl Iterator<Item = String>) -> usize
+{
+    let (i1, i2) = lines.map(|s| s.parse::<u32>().unwrap()).tee();
+    i1.zip(i2.skip(1))
+        .filter(|(prev, next)| next > prev)
+        .count()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    let stdin = io::stdin();
+    let input = stdin.lock().lines().map(|line| line.unwrap());
+    match opts {
+        Opts { day: 1, part: 1 } => println!("{}", day1_part1(input)),
+        _ => println!("Unimplemented: day{}, part{}", opts.day, opts.part),
+    }
 }
