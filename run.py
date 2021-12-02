7bff61218edd403809d8a80b82d9565e6c963e97
PowerSnail <hj@powersnail.com>
2021-12-02 11:10:36 -0800

    refactor python

--- run.py
+++ run.py
@@ -6,7 +6,18 @@ year = int(sys.argv[1])
 day = int(sys.argv[2])
 part = int(sys.argv[3])
 
+out_dir = pathlib.Path(f"outputs/{year}")
+out_dir.mkdir(parents=True, exist_ok=True)
+out_file = out_dir / f"day{day}-part{part}.txt"
+
+in_dir = pathlib.Path(f"inputs/{year}")
+in_dir.mkdir(parents=True, exist_ok=True)
+in_file = in_dir / f"day{day}.txt"
+
 sp.run("cargo build --release", shell=True, check=True, stdout=sp.PIPE, stderr=sp.PIPE)
-pathlib.Path(f"outputs/{year}").mkdir(parents=True, exist_ok=True)
-sp.run(f"target/release/aoc_{year} {day} {part} <inputs/{year}/day{day}.txt | tee outputs/{year}/day{day}-part{part}.txt", shell=True, check=True)
-sp.run(f"cat outputs/day{day}-part{part}.txt | xsel -ib", shell=True, check=True)
+sp.run(
+    f"target/release/aoc {year} {day} {part} <{in_file} | tee {out_file}",
+    shell=True,
+    check=True,
+)
+sp.run(f"cat {out_file} | xsel -ib", shell=True, check=True)
