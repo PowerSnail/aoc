7586a48fc5e192641c4991aac812ea47fe43c664
PowerSnail <hj@powersnail.com>
2021-12-03 18:43:24 -0800

    2015 day 6

--- src/aoc_2015.rs
+++ src/aoc_2015.rs
@@ -5,9 +5,15 @@
 use std::{collections::HashMap, io::Read};
 
 use itertools::Itertools;
-use nom::{bytes::complete::tag, multi::separated_list0, AsChar};
+use nom::{
+    branch::alt,
+    bytes::complete::{is_a, tag},
+    multi::separated_list0,
+    sequence::separated_pair,
+    AsChar, IResult,
+};
 
-use crate::parsers::parse_i64;
+use crate::parsers::{parse_i64, parse_usize};
 
 // --- Parsings ---
 
@@ -214,11 +220,84 @@ pub fn day5_part2(input: std::io::Stdin) {
         .count();
     println!("{}", count);
 }
-pub fn day6_part1(_: std::io::Stdin) {
-    todo!()
+
+type Rect = ((usize, usize), (usize, usize));
+
+#[derive(Debug)]
+enum Instruction {
+    TurnOn(Rect),
+    Toggle(Rect),
+    TurnOff(Rect),
 }
-pub fn day6_part2(_: std::io::Stdin) {
-    todo!()
+
+fn parse_instruction(line: &str) -> IResult<&str, Instruction> {
+    let (input, ins_type) = alt((tag("turn on "), tag("toggle "), tag("turn off ")))(line)?;
+    let mut point_parser = separated_pair(parse_usize, is_a(","), parse_usize);
+    let (input, coord_1) = point_parser(input)?;
+    let (input, _) = tag(" through ")(input)?;
+    let (input, coord_2) = point_parser(input)?;
+
+    let instruction = match ins_type {
+        "turn on " => Instruction::TurnOn((coord_1, coord_2)),
+        "turn off " => Instruction::TurnOff((coord_1, coord_2)),
+        "toggle " => Instruction::Toggle((coord_1, coord_2)),
+        _ => unreachable!(),
+    };
+    Ok((input, instruction))
+}
+
+macro_rules! rect_coords {
+    ($rect:expr) => {
+        (($rect.0 .0)..($rect.1.0 + 1)).cartesian_product(($rect.0 .1)..($rect.1.1 + 1))
+    };
+}
+
+pub fn day6_part1(input: std::io::Stdin) {
+    let count : u32 = input
+        .lines()
+        .map(Result::unwrap)
+        .map(|l| parse_instruction(&l).unwrap().1)
+        .fold(vec![0u32; 1000 * 1000], |mut grid, instruction| {
+            match instruction {
+                Instruction::Toggle(rect) => {
+                    rect_coords!(rect).for_each(|(x, y)| grid[x * 1000 + y] ^= 1)
+                }
+                Instruction::TurnOn(rect) => {
+                    rect_coords!(rect).for_each(|(x, y)| grid[x * 1000 + y] = 1)
+                }
+                Instruction::TurnOff(rect) => {
+                    rect_coords!(rect).for_each(|(x, y)| grid[x * 1000 + y] = 0)
+                }
+            };
+            grid
+        })
+        .into_iter()
+        .sum();
+    println!("{}", count);
+}
+
+pub fn day6_part2(input: std::io::Stdin) {
+    let count : i64 = input
+        .lines()
+        .map(Result::unwrap)
+        .map(|l| parse_instruction(&l).unwrap().1)
+        .fold(vec![0i64; 1000 * 1000], |mut grid, instruction| {
+            match instruction {
+                Instruction::Toggle(rect) => {
+                    rect_coords!(rect).for_each(|(x, y)| grid[x * 1000 + y] += 2)
+                }
+                Instruction::TurnOn(rect) => {
+                    rect_coords!(rect).for_each(|(x, y)| grid[x * 1000 + y] += 1)
+                }
+                Instruction::TurnOff(rect) => {
+                    rect_coords!(rect).for_each(|(x, y)| grid[x * 1000 + y] = (grid[x * 1000 + y] - 1).max(0))
+                }
+            };
+            grid
+        })
+        .into_iter()
+        .sum();
+    println!("{}", count);
 }
 pub fn day7_part1(_: std::io::Stdin) {
     todo!()
--- src/parsers.rs
+++ src/parsers.rs
@@ -11,3 +11,12 @@ pub fn parse_i64(line: &str) -> IResult<&str, i64> {
         Err(_) => fail(input),
     }
 }
+
+
+pub fn parse_usize(line: &str) -> IResult<&str, usize> {
+    let (input, digits) = take_while(is_digit)(line)?;
+    match digits.parse() {
+        Ok(n) => Ok((input, n)),
+        Err(_) => fail(input),
+    }
+}
