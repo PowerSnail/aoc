35a02a2b7f532ee8c07d7697e2b214f72b4b4478
PowerSnail <hj@powersnail.com>
2021-12-04 00:52:35 -0800

    day 4

--- src/aoc_2021.rs
+++ src/aoc_2021.rs
@@ -3,6 +3,8 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+use std::io::BufRead;
+
 use itertools::Itertools;
 use nom::character::complete::char;
 use nom::sequence::separated_pair;
@@ -196,14 +198,106 @@ pub fn day3_part2(input: std::io::Stdin) {
     println!("{}", o2_number * co2_number);
 }
 
-pub fn day4_part1(_: std::io::Stdin) {
-    // input.lines().map(Result::unwrap)
-    //     .filter(|l|)
-    todo!()
+fn has_bingo(board: &[u32]) -> bool {
+    if board
+        .iter()
+        .chunks(5)
+        .into_iter()
+        .any(|mut row| row.all(|n| *n == 0))
+    {
+        return true;
+    }
+    if (0usize..5).any(|c| (0..5).all(|r| board[r * 5 + c] == 0)) {
+        return true;
+    }
+    return false;
 }
 
-pub fn day4_part2(_: std::io::Stdin) {
-    todo!()
+pub fn day4_part1(input: std::io::Stdin) {
+    let mut lines = input.lines();
+    let num_sequence = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split(",")
+        .map(|string| string.parse::<u32>().unwrap())
+        .collect_vec();
+    let mut boards: Vec<u32> = lines
+        .map(Result::unwrap)
+        .map(|l| {
+            l.split(" ")
+                .filter_map(|l| l.parse::<u32>().ok())
+                .collect_vec()
+        })
+        .flatten()
+        .collect_vec();
+
+    let board_count = boards.len() / 25;
+    for n in num_sequence.into_iter() {
+        for x in boards.iter_mut() {
+            if *x == n {
+                *x = 0;
+            }
+        }
+
+        for board in 0..board_count {
+            if has_bingo(&boards[board * 25..(board + 1) * 25]) {
+                println!(
+                    "{}",
+                    boards[board * 25..(board + 1) * 25].iter().sum::<u32>() * n
+                );
+                return;
+            }
+        }
+    }
+}
+
+pub fn day4_part2(input: std::io::Stdin) {
+    let mut lines = input.lines();
+    let num_sequence = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split(",")
+        .map(|string| string.parse::<u32>().unwrap())
+        .collect_vec();
+    let mut boards: Vec<u32> = lines
+        .map(Result::unwrap)
+        .map(|l| {
+            l.split(" ")
+                .filter_map(|l| l.parse::<u32>().ok())
+                .collect_vec()
+        })
+        .flatten()
+        .collect_vec();
+
+    let board_count = boards.len() / 25;
+    let mut remaining_boards = (0..board_count).collect_vec();
+
+    for n in num_sequence.into_iter() {
+        for x in boards.iter_mut() {
+            if *x == n {
+                *x = 0;
+            }
+        }
+
+        let boards_left = remaining_boards
+            .iter()
+            .map(|board| *board)
+            .filter(|board| !has_bingo(&boards[board * 25..(board + 1) * 25]))
+            .collect_vec();
+
+        if boards_left.len() == 0 {
+            let board = remaining_boards[0];
+            println!(
+                "{}",
+                boards[board * 25..(board + 1) * 25].iter().sum::<u32>() * n
+            );
+            return;
+        }
+
+        remaining_boards = boards_left;
+    }
 }
 
 pub fn day5_part1(_: std::io::Stdin) {
