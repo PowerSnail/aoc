7f7e4dfdfac1f31e673a98b4139fe80380b0a231
PowerSnail <hj@powersnail.com>
2021-12-07 15:33:30 -0800

    2015 day10

--- src/aoc_2015.rs
+++ src/aoc_2015.rs
@@ -493,11 +493,43 @@ pub fn day9_part2() {
         .unwrap();
     println!("{}", max_distance);
 }
+
 pub fn day10_part1() {
-    todo!()
+    let init = std_iter!(Bytes).map(|b| (b - b'0') as u64).collect_vec();
+    let n = (0..40).fold(init, |v, _| {
+        let mut numbers = vec![v[0]];
+        let mut counts = vec![1u64];
+        for n in v.into_iter().skip(1) {
+            let last = numbers.len() - 1;
+            if n == numbers[last] {
+                counts[last] += 1;
+            } else {
+                numbers.push(n);
+                counts.push(1);
+            }
+        }
+        counts.into_iter().interleave(numbers.into_iter()).collect_vec()
+    });
+    println!("{}", n.len());
 }
+
 pub fn day10_part2() {
-    todo!()
+    let init = std_iter!(Bytes).map(|b| (b - b'0') as u64).collect_vec();
+    let n = (0..50).fold(init, |v, _| {
+        let mut numbers = vec![v[0]];
+        let mut counts = vec![1u64];
+        for n in v.into_iter().skip(1) {
+            let last = numbers.len() - 1;
+            if n == numbers[last] {
+                counts[last] += 1;
+            } else {
+                numbers.push(n);
+                counts.push(1);
+            }
+        }
+        counts.into_iter().interleave(numbers.into_iter()).collect_vec()
+    });
+    println!("{}", n.len());
 }
 pub fn day11_part1() {
     todo!()
