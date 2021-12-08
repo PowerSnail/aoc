1d9ce91b4f9837c902b09891128a3535d31be0b8
PowerSnail <hj@powersnail.com>
2021-12-08 15:12:03 -0800

    day8

--- run.py
+++ run.py
@@ -2,36 +2,55 @@ import subprocess as sp
 import sys
 import pathlib
 import functools
+import traceback
 
 shell = functools.partial(sp.run, shell=True, check=True, text=True)
 
-year = int(sys.argv[1])
-day = int(sys.argv[2])
-part = int(sys.argv[3])
-to_save = "-s" in sys.argv
-to_test = "-t" in sys.argv
 
-out_dir = pathlib.Path(f"outputs/{year}")
-out_dir.mkdir(parents=True, exist_ok=True)
-out_file = out_dir / f"day{day}-part{part}.txt"
+def main():
+    year = int(sys.argv[1])
+    day = int(sys.argv[2])
+    part = int(sys.argv[3])
+    to_save = "-s" in sys.argv
+    to_test = "-t" in sys.argv
 
-in_dir = pathlib.Path(f"inputs/{year}")
-in_dir.mkdir(parents=True, exist_ok=True)
-in_file = "inputs/test.txt" if to_test else in_dir / f"day{day}.txt" 
+    out_dir = pathlib.Path(f"outputs/{year}")
+    out_dir.mkdir(parents=True, exist_ok=True)
+    out_file = out_dir / f"day{day}-part{part}.txt"
 
-shell("cargo build --release", stdout=sp.DEVNULL, stderr=sp.DEVNULL)
-answer = shell(
-    f"target/release/aoc {year} {day} {part} <{in_file}",
-    stdout=sp.PIPE,
-    stderr=None,
-).stdout
+    in_dir = pathlib.Path(f"inputs/{year}")
+    in_dir.mkdir(parents=True, exist_ok=True)
+    in_file = "inputs/test.txt" if to_test else in_dir / f"day{day}.txt" 
 
-print(answer)
-shell("xsel -ib", input=answer)
+    shell("cargo build --release", stdout=sp.DEVNULL, stderr=sp.DEVNULL)
 
-if to_save:
-    with out_file.open("w") as file:
-        file.write(answer)
-else:
-    if out_file.exists():
-        process = shell(f"diff {out_file} - || true", input=answer)
+    result = shell(
+        f"target/release/aoc {year} {day} {part} <{in_file}",
+        stdout=sp.PIPE,
+        stderr=sp.PIPE,
+    )
+
+    answer = result.stdout
+
+    print(answer)
+    shell("xsel -ib", input=answer)
+
+    if to_save:
+        with out_file.open("w") as file:
+            file.write(answer)
+    else:
+        if out_file.exists():
+            shell(f"diff {out_file} - || true", input=answer)
+
+
+try:
+    main()
+except sp.CalledProcessError as err:
+    if err.stdout:
+        sys.stderr.write("STDOUT\n------\n")
+        sys.stderr.write(err.stdout)
+    if err.stderr:
+        sys.stderr.write("STDERR\n------\n")
+        sys.stderr.write(err.stderr)
+    sys.stderr.write("------\n")
+    raise err
--- src/aoc_2021.rs
+++ src/aoc_2021.rs
@@ -388,12 +388,84 @@ pub fn day7_part2() {
         .unwrap();
     println!("{}", total_diff);
 }
+
 pub fn day8_part1() {
-    todo!()
+    let count = std_iter!(Lines)
+        .map(|l| {
+            let (_, rhs) = l.split_once(" | ").unwrap();
+            rhs.split_whitespace()
+                .filter(|l| match l.len() {
+                    2 | 3 | 4 | 7 => true,
+                    _ => false,
+                })
+                .count()
+        })
+        .sum::<usize>();
+    println!("{}", count);
 }
+
+fn encode_char(s: u8) -> u8 {
+    1 << (s - b'a')
+}
+
+fn encode_lcd(s: &[u8]) -> (usize, u8) {
+    (s.len(), s.iter().fold(0, |n, b| n | encode_char(*b)))
+}
+
+fn pop_if<T, F>(vec: &mut Vec<T>, f: F) -> T
+where
+    F: Fn(&T) -> bool,
+{
+    let (x, _) = vec.iter().find_position(|&x| f(x)).expect("Not found");
+    vec.remove(x)
+}
+
 pub fn day8_part2() {
-    todo!()
+    let result = std_iter!(Lines)
+        .map(|l| {
+            let (lhs, rhs) = l.split_once(" | ").unwrap();
+
+            let mut patterns = lhs
+                .split_whitespace()
+                .map(|l| encode_lcd(l.as_bytes()))
+                .fold(vec![vec![]; 8], |mut v, (count, pattern)| {
+                    v[count].push(pattern);
+                    v
+                });
+            let mut representations = [0; 10];
+
+            representations[1] = patterns[2][0];
+            representations[7] = patterns[3][0];
+            representations[4] = patterns[4][0];
+            representations[8] = patterns[7][0];
+
+            let four_angle = representations[4] - representations[1];
+            let one = representations[1];
+
+            representations[5] = pop_if(&mut patterns[5], |x| x & four_angle == four_angle);
+            representations[3] = pop_if(&mut patterns[5], |x| x & one == one);
+            representations[2] = patterns[5][0];
+            representations[0] = pop_if(&mut patterns[6], |x| x & four_angle != four_angle);
+            representations[9] = pop_if(&mut patterns[6], |x| x & one == one);
+            representations[6] = patterns[6][0];
+
+            rhs.split_whitespace()
+                .map(|token| {
+                    let (_, pattern) = encode_lcd(token.as_bytes());
+                    let (i, _) = representations
+                        .iter()
+                        .find_position(|&&x| x == pattern)
+                        .unwrap();
+                    i
+                })
+                .fold1(|sum, i| sum * 10 + i)
+                .unwrap()
+        })
+        .sum::<usize>();
+
+    println!("{}", result);
 }
+
 pub fn day9_part1() {
     todo!()
 }
