2d2ea504c13a53cf264f5834a652bf7f9a7b5b61
PowerSnail <hj@powersnail.com>
2021-12-10 00:35:02 -0800

    day10

--- src/aoc_2021.rs
+++ src/aoc_2021.rs
@@ -460,7 +460,7 @@ pub fn day8_part2() {
                         .unwrap();
                     i
                 })
-                .fold1(|sum, i| sum * 10 + i)
+                .reduce(|sum, i| sum * 10 + i)
                 .unwrap()
         })
         .sum::<usize>();
@@ -541,12 +541,80 @@ pub fn day9_part2() {
     println!("{}", product);
 }
 
+enum LineProcessResult {
+    IllegalChar(u8),
+    UnmatchedChars(Vec<u8>),
+}
+
+fn left_bracket_of(c: u8) -> u8 {
+    match c {
+        b')' => b'(',
+        b']' => b'[',
+        b'}' => b'{',
+        b'>' => b'<',
+        _ => unreachable!(),
+    }
+}
+
+fn process_line(s: &str) -> LineProcessResult {
+    let mut stack = vec![];
+    for c in s.bytes() {
+        match c {
+            b'(' | b'[' | b'{' | b'<' => {
+                stack.push(c);
+            }
+            _ => {
+                if stack.pop().unwrap() != left_bracket_of(c) {
+                    return LineProcessResult::IllegalChar(c);
+                }
+            }
+        }
+    }
+    LineProcessResult::UnmatchedChars(stack)
+}
+
 pub fn day10_part1() {
-    todo!()
+    let score = std_iter!(Lines)
+        .filter_map(|l| match process_line(&l) {
+            LineProcessResult::IllegalChar(c) => Some(c),
+            _ => None,
+        })
+        .map(|char| match char {
+            b')' => 3,
+            b']' => 57,
+            b'}' => 1197,
+            b'>' => 25137,
+            _ => unreachable!(),
+        })
+        .sum::<u64>();
+    println!("{}", score);
 }
+
 pub fn day10_part2() {
-    todo!()
+    let mut scores = std_iter!(Lines)
+        .filter_map(|l| match process_line(&l) {
+            LineProcessResult::UnmatchedChars(chars) => Some(chars),
+            _ => None,
+        })
+        .map(|remaining_chars| {
+            remaining_chars
+                .into_iter()
+                .rev()
+                .map(|char| match char {
+                    b'(' => 1,
+                    b'[' => 2,
+                    b'{' => 3,
+                    b'<' => 4,
+                    _ => unreachable!(),
+                })
+                .fold(0u64, |sum, x| sum * 5 + x)
+        })
+        .collect_vec();
+
+    scores.sort_unstable();
+    println!("{}", scores[scores.len() / 2]);
 }
+
 pub fn day11_part1() {
     todo!()
 }
