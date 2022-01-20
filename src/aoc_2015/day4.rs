d92f293c65faeca1626fcb73bb63d035886fef09
PowerSnail <hj@powersnail.com>
2022-01-20 13:03:50 -0800

    improve efficiency of day4

--- src/aoc_2015/day4.rs
+++ src/aoc_2015/day4.rs
@@ -1,31 +1,30 @@
 use crate::{prelude::*, std_iter};
 
+fn collision(key: &[u8], target: &[u8]) -> i32 {
+    let l = (target.len() + 1) / 2;
+    (1..i32::MAX)
+        .find(|i| {
+            let crack = [key, i.to_string().as_bytes()].concat();
+            let digest = md5::compute(&crack);
+            let prefix = &digest[..l];
+            let mut prefix_hex = vec![];
+            for i in prefix {
+                prefix_hex.push(i & 0xF0);
+                prefix_hex.push(i & 0x0F);
+            }
+            prefix_hex.starts_with(target)
+        })
+        .unwrap()
+}
+
 pub fn part1() {
     let key: Vec<u8> = std_iter!(Bytes).filter(|c| c.is_alphanum()).collect();
-
-    let i = (1..i32::MAX)
-        .filter(|i| {
-            let crack = [&key, i.to_string().as_bytes()].concat();
-            let digest = md5::compute(&crack);
-            format!("{:x}", digest).starts_with("00000")
-        })
-        .next()
-        .unwrap();
-
+    let i = collision(&key, &[0, 0, 0, 0, 0]);
     println!("{}", i);
 }
 
 pub fn part2() {
     let key: Vec<u8> = std_iter!(Bytes).filter(|c| c.is_alphanum()).collect();
-
-    let i = (1..i32::MAX)
-        .filter(|i| {
-            let crack = [&key, i.to_string().as_bytes()].concat();
-            let digest = md5::compute(&crack);
-            format!("{:x}", digest).starts_with("000000")
-        })
-        .next()
-        .unwrap();
-
+    let i = collision(&key, &[0, 0, 0, 0, 0, 0]);
     println!("{}", i);
 }
