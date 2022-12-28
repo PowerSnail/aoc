672e33ee4d931c4ffa7f51b119600e0f57b69d88
PowerSnail <hj@powersnail.com>
2022-12-28 15:14:59 -0800

    day 15

--- src/aoc_2022/day15.rs
+++ src/aoc_2022/day15.rs
@@ -27,42 +27,63 @@ fn parse_input(i: &str) -> ParseResult<Vec<Report>> {
 // const TARGET_ROW: i64 = 10;
 const TARGET_ROW: i64 = 2_000_000;
 
-pub fn part1() {
-    let reports = parse_input(stdio_string().as_str()).unwrap().1;
-    let mut existing: HashSet<i64> = HashSet::new();
-    let mut dead_zones = Vec::new();
+// const CONSTRAINT: i64 = 20;
+const CONSTRAINT: i64 = 4_000_000;
 
-    for report in reports {
-        let distance = report.distance();
-        let horizontal_max_delta = distance - (report.sensor.1 - TARGET_ROW).abs();
-        if horizontal_max_delta >= 0 {
-            let range = (report.sensor.0 - horizontal_max_delta, report.sensor.0 + horizontal_max_delta + 1);
-            dead_zones.push(range);
-        }
-
-        if report.beacon.1 == TARGET_ROW {
-            existing.insert(report.beacon.0);
-        }
+fn merge_ranges(mut ranges: impl Iterator<Item=(i64, i64)>) -> Vec<(i64, i64)> {
+    let mut merged_dead_zones = Vec::new();
+    if let Some(first) = ranges.next() {
+        merged_dead_zones.push(first);
     }
 
-    dead_zones.sort();
-    let mut merged_dead_zones = Vec::new();
-    merged_dead_zones.push(dead_zones[0]);
-    
-    for (lo, hi)  in dead_zones.into_iter().skip(1) {
-        let latest = merged_dead_zones.pop().unwrap();
-        if lo <= latest.1 {
-            merged_dead_zones.push((latest.0, hi))
+    for (lo, hi)  in ranges {
+        let (lo_prev, hi_prev) = merged_dead_zones.pop().unwrap();
+        if lo <= hi_prev {
+            merged_dead_zones.push((lo_prev, hi.max(hi_prev)))
         } else {
-            merged_dead_zones.push(latest);
+            merged_dead_zones.push((lo_prev, hi_prev));
             merged_dead_zones.push((lo, hi));
         }
     }
+    merged_dead_zones
+}
 
+fn dead_zone(report: &Report, row: i64) -> Option<(i64, i64)> {
+    let distance = report.distance();
+    let horizontal_max_delta = distance - (report.sensor.1 - row).abs();
+    if horizontal_max_delta < 0 {
+        None
+    } else {
+        let range = (report.sensor.0 - horizontal_max_delta, report.sensor.0 + horizontal_max_delta + 1);
+        Some(range)
+    }
+}
+
+pub fn part1() {
+    let reports = parse_input(stdio_string().as_str()).unwrap().1;
+
+    let dead_zones = reports.iter().filter_map(|r| dead_zone(r, TARGET_ROW)).sorted();
+    let merged_dead_zones = merge_ranges(dead_zones);
+
+    let existing = reports.iter().filter(|r| r.beacon.1 == TARGET_ROW).map(|r| r.beacon.0).collect::<HashSet::<_>>().len();
     let total: i64 = merged_dead_zones.into_iter().map(|(lo, hi)| hi - lo).sum();
-
-    println!("{}", total as usize - existing.len());
+    println!("{}", total as usize - existing);
 }
 
 pub fn part2() {
+    let reports = parse_input(stdio_string().as_str()).unwrap().1;
+    for y in 0..=CONSTRAINT {
+        let dead_zones = reports.iter().filter_map(|r| dead_zone(r, y))
+            .map(|(lo, hi)| (lo.clamp(0, CONSTRAINT), hi.clamp(0, CONSTRAINT)))
+            .sorted();
+        let merged_dead_zones = merge_ranges(dead_zones);
+
+        if merged_dead_zones.len() == 1 {
+            continue;
+        }
+        let x = merged_dead_zones[0].1;
+        println!("{}", x * 4000000 + y);
+        return;
+    }
+    unreachable!();
 }
